9f79b31c10f058d087ff23adcbc9a54fba75b1ca
Chi-G <chijindu.nwokeohuru@gmail.com>
2025-06-04 13:27:18 +0100

    fix temperature conversion formula structure

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -11,7 +11,7 @@ def convert_to_celsius(fahrenheit):
     Returns:
         float: Temperature in Celsius
     """
-    return FAHRENHEIT_TO_CELSIUS_FACTOR * (fahrenheit - 32)
+    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
 def convert_to_fahrenheit(celsius):
     """
@@ -23,7 +23,7 @@ def convert_to_fahrenheit(celsius):
     Returns:
         float: Temperature in Fahrenheit
     """
-    return (CELSIUS_TO_FAHRENHEIT_FACTOR * celsius) + 32
+    return celsius * CELSIUS_TO_FAHRENHEIT_FACTOR + 32
 
 def main():
     try:
